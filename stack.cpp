a5c8a3af11c0ec3f4f4039c5259a059db2a5f3b6
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-05 16:04:48 +0700

    add default : Invalid choice

--- stack.cpp
+++ stack.cpp
@@ -111,7 +111,8 @@ int main()
         case 4:
              cout << "Exiting program." << endl;
              break;
-        
+        default:
+             cout << "Invalid choice. Try again." << endl;
 
         }
 
